7adf3c5cd6c3e6071c28f4b5bfc1a511900778e2
sikma0606-debug <sikma0606@gmail.com>
2026-03-10 15:31:30 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'c00fec6373a6a91e91cc5dd1f701433a';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
@@ -119,5 +119,6 @@ const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
       displayList(tvShows, 'tvshows-list');
       displayList(anime, 'anime-list');
     }
+
 
     init();
